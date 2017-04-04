fe0267cd060682edb13ae1a5d4074143eb847d10
Ciprian Ciubotariu <cheepeero@gmx.net>
2017-04-04 16:41:45 +0300

    Add jms consumer and jms producer projects

---
+++ jmsproducer/src/main/java/org/apifocal/silkmq/samples/jmsproducer/App.java
@@ -0,0 +1,13 @@
+package org.apifocal.silkmq.samples.jmsproducer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
